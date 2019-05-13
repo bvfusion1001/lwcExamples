e1d4e4652512c9668208f35cd04bd34cb3c0ca71
Brad Vidal <Brad.Vidal@Nintex.com>
2019-05-13 00:20:55 -0700

    Add dummy data to both components.

--- force-app/main/default/lwc/contactListLwc/contactListLwc.js
+++ force-app/main/default/lwc/contactListLwc/contactListLwc.js
@@ -1,3 +1,5 @@
 import { LightningElement } from 'lwc';
 
-export default class ContactListLwc extends LightningElement {}
+export default class ContactListLwc extends LightningElement {
+    contacts = [{Id:'1',Name:'one'},{Id:'2',Name:'two'},{Id:'3',Name:'three'}];
+}
