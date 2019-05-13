721d3a035efa1f4fda9367af803bdd718219984b
Brad Vidal <Brad.Vidal@Nintex.com>
2019-05-13 00:34:03 -0700

    Expose recordId to both components.

--- force-app/main/default/lwc/contactListLwc/contactListLwc.js
+++ force-app/main/default/lwc/contactListLwc/contactListLwc.js
@@ -1,6 +1,8 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, api } from 'lwc';
 import { getContacts } from 'c/dummyContactsProviderLwc';
 
 export default class ContactListLwc extends LightningElement {
+    @api recordId;
+    
     @track contacts = getContacts();
 }
